5574bdf255c285a2f21fa4bf232b4214c67d338e
Michael Ugochukwu <mark.omulu@gmail.com>
2022-07-23 11:53:43 +0100

    find bug

---
+++ person.rb
@@ -0,0 +1,58 @@
+require './nameable'
+
+class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def correct_name
+    name
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
+
+class Decorator < Nameable
+  attr_accessor :component
+
+  def initialize(component)
+    super
+    @component = component
+  end
+
+  def correct_name
+    @component.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @component.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @component.correct_name.length > 10 ? @component.correct_name[0..9] : @component.correct_name
+  end
+end
---
+++ student.rb
@@ -0,0 +1,18 @@
+$LOAD_PATH << '.'
+require 'person'
+
+class Student < Person
+  def initialize(age, name, classroom, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
+  end
+end
